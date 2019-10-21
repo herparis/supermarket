e1c39230aac1dffc1f9d624f7cffcce72a01745b
hernan paris <hernanleonparis@gmail.com>
2019-10-21 18:25:10 -0300

    Fix modal container

--- app/src/components/addModal/addModalContainer.js
+++ app/src/components/addModal/addModalContainer.js
@@ -2,12 +2,11 @@ import styled from 'styled-components'
 
 const AddModalContainer = styled.div`
   background-color: rgba(0, 0, 0, .78);
-  position: absolute;
+  position: fixed;
   top: 0;
   bottom: 0;
   right: 0;
   left: 0;
-  display: flex;
   align-items: center;
   justify-content: center;
 `
